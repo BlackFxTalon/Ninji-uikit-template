0abf4d95f60ada6940f14e29cae20bace4a2b7ea
BlackFxTalon <andyv@inbox.ru>
2019-09-12 21:25:45 +0600

    first commit

---
+++ src/scripts/plugins/uikit.js
@@ -0,0 +1,24 @@
+//  UIkit's JavaScript
+import Uikit from "uikit/dist/js/uikit";
+
+// Stripped down JavaScript. Core functionality without additional components
+// import UikitCore from "uikit/dist/js/uikit-core";
+
+// uikit icons
+import UiIcons from "uikit/dist/js/uikit-icons";
+
+// Components
+import Countdown from "uikit/dist/js/components/countdown";
+import Filter from "uikit/dist/js/components/filter";
+import Lightbox from "uikit/dist/js/components/lightbox";
+import LightboxPanel from "uikit/dist/js/components/lightbox-panel";
+import Notification from "uikit/dist/js/components/notification";
+import Parallax from "uikit/dist/js/components/parallax";
+import ParallaxSlider from "uikit/dist/js/components/slider-parallax";
+import Slider from "uikit/dist/js/components/slider";
+import Slideshow from "uikit/dist/js/components/slideshow";
+import Sortable from "uikit/dist/js/components/sortable";
+import Tooltip from "uikit/dist/js/components/tooltip";
+import Upload from "uikit/dist/js/components/upload";
+import Sortable from "uikit/dist/js/components/sortable";
+
